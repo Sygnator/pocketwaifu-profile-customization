be0fc2573fa162b14c24ac927f96d97546dcd50d
Sygnator <mateusz@sygnator.com>
2024-03-22 23:01:41 +0100

    feat: add darkmode toggle button

---
+++ src/lib/components/DarkModeButton.svelte
@@ -0,0 +1,6 @@
+<!-- src/lib/components/DarkModeButton.svelte -->
+<script>
+	import { DarkMode } from 'flowbite-svelte';
+</script>
+
+<DarkMode class="border text-primary-500 dark:border-gray-800 dark:text-primary-600" />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
+<!-- src/routes/+layout.svelte -->
 <script>
 	import '../app.pcss';
+
+	import DarkModeButton from '$lib/components/DarkModeButton.svelte';
 </script>
 
+<DarkModeButton />
 <slot />
