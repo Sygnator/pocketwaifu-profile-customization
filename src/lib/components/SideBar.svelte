1cf59e685c28c8709369cbd6cbb8f601c576d8bd
Sygnator <mateusz@sygnator.com>
2024-04-10 22:54:45 +0200

    fix: display karma state button on stats profile type

--- src/lib/components/SideBar.svelte
+++ src/lib/components/SideBar.svelte
@@ -196,6 +196,24 @@
 				<Toggle checked={$profileConfig.flip} on:change={profileConfig.switchFlip}>
 					Odwróć układ
 				</Toggle>
+				<SidebarItem label="Karma" {spanClass} on:click={changeKarma}>
+					<svelte:fragment slot="icon">
+						<img src={`/profile_assets/${$profileConfig.karma}.png`} alt="karma" />
+					</svelte:fragment>
+					<svelte:fragment slot="subtext">
+						<span
+							class="ms-3 inline-flex items-center justify-center rounded-full bg-gray-200 px-2 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300"
+						>
+							{#if $profileConfig.karma == KarmaState.Angel}
+								Dodatnia
+							{:else if $profileConfig.karma == KarmaState.Demon}
+								Ujemna
+							{:else}
+								Neutralna
+							{/if}
+						</span>
+					</svelte:fragment>
+				</SidebarItem>
 			</SidebarGroup>
 		{/if}
 
